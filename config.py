1a7007223ad4960f3ccce8b001e240f9be3282ed
danger_videograph <78549442+NickStS@users.noreply.github.com>
2025-10-27 01:39:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-TOKEN = "6109191098:AAFSM0g93KqeazjwirWyLnA6TJKn15UEngU"  # obtained from @BotFather
+TOKEN = "TOKEN_BOTFATHER"  # @BotFather
 # obtained from https://platform.openai.com/account/api-keys
-OPENAI_TOKEN = "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"
+
+OPENAI_TOKEN = "TOKEN_OPENAI"
